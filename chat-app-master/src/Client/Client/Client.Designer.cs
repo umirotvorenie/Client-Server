9f14c21ef82deb0b694254d2f6b94473080eb44e
Kate <138885230+umirotvorenie@users.noreply.github.com>
2024-04-25 20:36:27 +0300

    Add files via upload

---
+++ chat-app-master/src/Client/Client/Client.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectButton = new System.Windows.Forms.Button();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.localaddrLabel = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.sendTextBox = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.addrTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(13, 13);
+            this.connectButton.Margin = new System.Windows.Forms.Padding(4);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(116, 28);
+            this.connectButton.TabIndex = 28;
+            this.connectButton.TabStop = false;
+            this.connectButton.Text = "Войти";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(412, 21);
+            this.portLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(32, 13);
+            this.portLabel.TabIndex = 27;
+            this.portLabel.Text = "Порт";
+            // 
+            // localaddrLabel
+            // 
+            this.localaddrLabel.AutoSize = true;
+            this.localaddrLabel.Location = new System.Drawing.Point(213, 18);
+            this.localaddrLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.localaddrLabel.Name = "localaddrLabel";
+            this.localaddrLabel.Size = new System.Drawing.Size(44, 13);
+            this.localaddrLabel.TabIndex = 26;
+            this.localaddrLabel.Text = "Адресс";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(449, 18);
+            this.portTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.portTextBox.MaxLength = 10;
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(132, 20);
+            this.portTextBox.TabIndex = 25;
+            this.portTextBox.TabStop = false;
+            this.portTextBox.Text = "3306";
+            this.portTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.Color.PowderBlue;
+            this.logTextBox.Location = new System.Drawing.Point(63, 99);
+            this.logTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(566, 300);
+            this.logTextBox.TabIndex = 30;
+            this.logTextBox.TabStop = false;
+            // 
+            // sendTextBox
+            // 
+            this.sendTextBox.BackColor = System.Drawing.Color.LightBlue;
+            this.sendTextBox.Location = new System.Drawing.Point(63, 369);
+            this.sendTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.sendTextBox.Multiline = true;
+            this.sendTextBox.Name = "sendTextBox";
+            this.sendTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.sendTextBox.Size = new System.Drawing.Size(566, 30);
+            this.sendTextBox.TabIndex = 32;
+            this.sendTextBox.TabStop = false;
+            this.sendTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SendTextBox_KeyDown);
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(213, 49);
+            this.usernameLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(29, 13);
+            this.usernameLabel.TabIndex = 36;
+            this.usernameLabel.Text = "Имя";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(269, 46);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.usernameTextBox.MaxLength = 50;
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(132, 20);
+            this.usernameTextBox.TabIndex = 35;
+            this.usernameTextBox.TabStop = false;
+            this.usernameTextBox.Text = "Клиент";
+            this.usernameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // addrTextBox
+            // 
+            this.addrTextBox.Location = new System.Drawing.Point(268, 18);
+            this.addrTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.addrTextBox.MaxLength = 200;
+            this.addrTextBox.Name = "addrTextBox";
+            this.addrTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.addrTextBox.Size = new System.Drawing.Size(132, 20);
+            this.addrTextBox.TabIndex = 39;
+            this.addrTextBox.TabStop = false;
+            this.addrTextBox.Text = "127.0.0.1";
+            this.addrTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(10, 376);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "Имя";
+            // 
+            // Client
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addrTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.sendTextBox);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.localaddrLabel);
+            this.Controls.Add(this.portTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Client";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Клиент";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.Label localaddrLabel;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.TextBox sendTextBox;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox addrTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ chat-app-master/src/Server/Server/Server.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.localaddrLabel = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.sendTextBox = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.addrTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(13, 13);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(116, 28);
+            this.startButton.TabIndex = 23;
+            this.startButton.TabStop = false;
+            this.startButton.Text = "Запустить";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.BackColor = System.Drawing.Color.Transparent;
+            this.portLabel.Location = new System.Drawing.Point(410, 21);
+            this.portLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(32, 13);
+            this.portLabel.TabIndex = 22;
+            this.portLabel.Text = "Порт";
+            // 
+            // localaddrLabel
+            // 
+            this.localaddrLabel.AutoSize = true;
+            this.localaddrLabel.BackColor = System.Drawing.Color.Transparent;
+            this.localaddrLabel.Location = new System.Drawing.Point(210, 21);
+            this.localaddrLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.localaddrLabel.Name = "localaddrLabel";
+            this.localaddrLabel.Size = new System.Drawing.Size(38, 13);
+            this.localaddrLabel.TabIndex = 21;
+            this.localaddrLabel.Text = "Адрес";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(447, 18);
+            this.portTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.portTextBox.MaxLength = 10;
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(132, 20);
+            this.portTextBox.TabIndex = 20;
+            this.portTextBox.TabStop = false;
+            this.portTextBox.Text = "3306";
+            this.portTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.logTextBox.Location = new System.Drawing.Point(63, 99);
+            this.logTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(566, 300);
+            this.logTextBox.TabIndex = 24;
+            this.logTextBox.TabStop = false;
+            // 
+            // sendTextBox
+            // 
+            this.sendTextBox.Location = new System.Drawing.Point(63, 369);
+            this.sendTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.sendTextBox.Multiline = true;
+            this.sendTextBox.Name = "sendTextBox";
+            this.sendTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.sendTextBox.Size = new System.Drawing.Size(566, 30);
+            this.sendTextBox.TabIndex = 27;
+            this.sendTextBox.TabStop = false;
+            this.sendTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SendTextBox_KeyDown);
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.usernameLabel.Location = new System.Drawing.Point(210, 49);
+            this.usernameLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(29, 13);
+            this.usernameLabel.TabIndex = 33;
+            this.usernameLabel.Text = "Имя";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(266, 46);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.usernameTextBox.MaxLength = 50;
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(132, 20);
+            this.usernameTextBox.TabIndex = 34;
+            this.usernameTextBox.TabStop = false;
+            this.usernameTextBox.Text = "Сервер";
+            this.usernameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.usernameTextBox.TextChanged += new System.EventHandler(this.usernameTextBox_TextChanged);
+            // 
+            // addrTextBox
+            // 
+            this.addrTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.addrTextBox.Location = new System.Drawing.Point(266, 18);
+            this.addrTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.addrTextBox.MaxLength = 200;
+            this.addrTextBox.Name = "addrTextBox";
+            this.addrTextBox.Size = new System.Drawing.Size(132, 20);
+            this.addrTextBox.TabIndex = 37;
+            this.addrTextBox.TabStop = false;
+            this.addrTextBox.Text = "127.0.0.1";
+            this.addrTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(10, 376);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Имя";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Server
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addrTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.sendTextBox);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.localaddrLabel);
+            this.Controls.Add(this.portTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Server";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сервер";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Server_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.Label localaddrLabel;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.TextBox sendTextBox;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox addrTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
